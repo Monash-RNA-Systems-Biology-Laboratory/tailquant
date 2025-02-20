d708593e284ef4343fbc0018d9b774b79c7a6f7d
Paul Harrison <paul.harrison@monash.edu>
2025-02-21 08:12:07 +1100

    max_tail control in shiny app

--- R/shiny_site_examiner.R
+++ R/shiny_site_examiner.R
@@ -1,11 +1,14 @@
 
-site_examiner_ui <- function(title) {
+site_examiner_ui <- function(tq, title) {
+    max_tail_upper <- get_attr(tq$sites, "max_tail")
+    
     ui <- shiny::fluidPage(
         shiny::titlePanel(title),
         shiny::sidebarLayout(
             shiny::sidebarPanel(
                 shiny::numericInput("top_n", "List this many top sites by n_tail_ended (0=all)", value=0, min=0, step=1),
                 shiny::numericInput("heatmap_rows", "Show this many sites in heatmap", value=50, min=1, max=2000, step=1),
+                shiny::numericInput("max_tail", "Maximum tail length in plots", value=max_tail_upper, min=1, max=max_tail_upper, step=1),
                 shiny::numericInput("step", "Density plot bin size", value=1, min=1, step=1),
                 shiny::checkboxInput("show_samples", "Show individual samples in plots.", value=TRUE),
                 shiny::checkboxInput("assume_all_died", "Use old method, treating tail-to-end-of-read as actual tail length.")
@@ -101,7 +104,7 @@ site_examiner_server <- function(tq, input,output,session) {
     plot_server("survival_plot", \() {
         p <- plot_km_survival(selected_kms()$km, selected_kms()$name, 
             min_tail=get_attr(sites, "min_tail", 0),
-            max_tail=get_attr(sites, "max_tail"))
+            max_tail=input$max_tail)
         if (input$show_samples) 
             p <- p + ggplot2::scale_color_discrete(type=samples$color)
         else
@@ -113,7 +116,7 @@ site_examiner_server <- function(tq, input,output,session) {
     plot_server("density_plot", \() {
         p <- plot_km_density(selected_kms()$km, selected_kms()$name, 
             min_tail=get_attr(sites, "min_tail", 0),
-            max_tail=get_attr(sites, "max_tail"),
+            max_tail=input$max_tail,
             step=input$step)
         if (input$show_samples) 
             p <- p + ggplot2::scale_color_discrete(type=samples$color)
@@ -126,7 +129,7 @@ site_examiner_server <- function(tq, input,output,session) {
     plot_server("density_heatmap", \() {
         p <- plot_km_density_heatmap(selected_kms()$km, selected_kms()$name, 
             min_tail=get_attr(sites, "min_tail", 0),
-            max_tail=get_attr(sites, "max_tail"),
+            max_tail=input$max_tail,
             step=input$step,
             normalize_max=FALSE)
         print(p)
@@ -158,7 +161,7 @@ site_examiner_server <- function(tq, input,output,session) {
         
         p <- plot_km_density_heatmap(kms, names,
             min_tail=get_attr(sites, "min_tail", 0),
-            max_tail=get_attr(sites, "max_tail"),
+            max_tail=input$max_tail,
             step=input$step)
         print(p)
     })
@@ -181,7 +184,7 @@ site_examiner_server <- function(tq, input,output,session) {
 #' @export
 shiny_site_examiner <- function(tq, title="Tail distribution examiner") {
     shiny::shinyApp(
-        site_examiner_ui(title)
+        site_examiner_ui(tq, title)
         ,\(...) site_examiner_server(tq, ...)
     )
 }
